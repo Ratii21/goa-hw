b5461a28986c6716673b22da0cdd1536a4d7c5a2
rati21 <ratibandelidze09@gmail.com>
2024-02-26 22:12:53 +0400

    commit

---
+++ day 4/homework4.py
@@ -0,0 +1,4 @@
+
+number1 = input("enter a number: ")
+number2 = input("enter a number: ")
+print(int(number1) + int(number2))
---
+++ day 4/homework5.py
@@ -0,0 +1,4 @@
+name = input("enter your name: ")
+age = input("enter your age: ")
+question = input("enter how many years into the future do you want to travel: ")
+print(int(age) + int(question))
