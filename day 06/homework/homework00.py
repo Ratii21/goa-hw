be0900eb68d106e93d91b1b242bd5e37cdaeea97
rati21 <ratibandelidze09@gmail.com>
2024-03-07 14:39:58 +0400

    commit

---
+++ day 06/homework/homework00.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter any number :"))
+num2 = int(input("enter any number :"))
+num3 = int(input("enter any number :"))
+print(num1 + num2 + num3)
+print(num1 * num2 -num3)
+print(num1 / num2 + num3)
+print(num1 + num2 - num3 * (num1 + num3) / num1)
