890aaf9f5581247bf8ddd739cf02fb1cb961cb21
rati21 <ratibandelidze09@gmail.com>
2024-03-14 18:53:36 +0400

    commit

---
+++ day 12/homework/day 09/classwork/day09.py
@@ -0,0 +1,21 @@
+pushups_target = 10
+situps_target = 5
+
+done_pushups = 10
+done_situps = 5
+
+print(done_pushups == pushups_target and done_situps == situps_target)
+
+# გაომიტანს True - ს
+
+
+print(10 == 10 and 5 == 10)
+
+# გამოიტანს False - ს
+
+
+# iteration ნიშნავს გამეორებას
+
+# sequences ნიშნავს თანმიმდევრობას
+
+# selection ნიშნავს არჩევანს
--- day 12/homework/homework04.py
+++
@@ -1,7 +0,0 @@
-min = int(input("Enter the minimum value for the range: "))
-max = int(input("Enter the maximum value for the range: "))
-
-print("Prime numbers between" ,min, "and" ,max, "are: ")
-
-for i in range(min, max + 1):
-    print("i")
