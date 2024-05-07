73903f4e219ef7dd85d9d867d1854a44d8088ea2
rati21 <ratibandelidze09@gmail.com>
2024-05-07 22:10:47 +0400

    commit

--- python/day 28/homework/homework03.py
+++ python/day 28/homework/homework03.py
@@ -1,10 +1,3 @@
 def solution(text, ending):
-    
-    
-    for i in range(len(text)-1,-1,-1):
-        for b in range(len(ending)-1,-1,-1):
-            if text[i] == ending[b]:
-                return True
-            elif text[i] != ending[b]:
-                return False
-
+    return text.endswith(ending)
+   
