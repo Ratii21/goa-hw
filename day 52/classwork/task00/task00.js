e8d8f70d04b38afc85e6afa1420224f07f68815b
Ratii21 <rbandelidze@gmail.com>
2024-08-02 20:49:08 +0400

    commit'

---
+++ day 52/classwork/task00/task00.js
@@ -0,0 +1,23 @@
+let x = 5
+let y = 10
+console.log(x + y)
+console.log(y - x)
+console.log(x * y)
+console.log(y / x)
+console.log(x % y)
+console.log(x + y - 5)
+
+let a = 0
+a = 1
+a = 2
+a = 3
+a = 4
+a = 5
+a = 6
+a = 7
+a = 8
+a = 9
+a = 10
+
+console.log(a)
+
