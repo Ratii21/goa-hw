b7303f459d1c45014319e3c91a40316fb5c15bd3
Ratii21 <rbandelidze@gmail.com>
2024-08-08 10:11:04 +0400

    commit

---
+++ day 54/classwork/task01/task01.js
@@ -0,0 +1,33 @@
+// //1
+// function error()
+// return 5 + 2
+
+// //2
+// consolelog(error)
+
+// //3
+// #error
+
+// //4
+// let 1error = "error"
+
+// //5
+// let err or = "error"
+
+// //6
+// let erro r = "error"
+
+// //7
+// function error {
+//     return "error"
+// }
+
+// //8
+// Hello = "hello"
+
+// //9
+// def error():
+//     return error
+
+// //10
+// print("error")
---
+++ day 54/classwork/task02/task02.js
@@ -0,0 +1,18 @@
+let red = document.getElementById("p1")
+red.style.color = "red"
+
+let blue = document.getElementById("p2")
+blue.style.color = "blue"
+
+let green = document.getElementById("p3")
+green.style.color = "green"
+
+let orange = document.getElementById("p4")
+orange.style.color = "orange"
+
+let pink = document.getElementById("p5")
+pink.style.color = "pink"
+
+
+
+
