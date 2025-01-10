2435ede040b2e67032f943ba3070718db21803a0
Ratii21 <ratibandelidze09@gmail.com>
2025-01-10 14:13:02 +0400

    commit

---
+++ day 70/classwork/task00.js
@@ -0,0 +1,6 @@
+let firstName = "Rati"
+let secondName = "Bandelidze"
+let age = "15"
+let nation = "Georgia"
+console.log("I am " + firstName + " " + secondName + ", I am " + age + " years old and I am from " + nation)
+alert(("I am " + firstName + " " + secondName + ", I am " + age + " years old and I am from " + nation))
---
+++ day 70/homework/task01.js
@@ -0,0 +1,6 @@
+const firstName = "Rati"
+const secondtName = "Bandelidze"
+const nation = "Georgia" 
+let age = "15"
+let height = 1.77
+let weight = 60
